0d3e68090ca3f2557ad2b7ed1c93bc2a4968cee9
Ubaidullah Muhammad Ahmad <ubaidullahmahmad@gmail.com>
2022-09-10 03:30:22 -0700

    alx

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- ** main - main entry
+ *  * main -Entry point
  *
- ** Return: Always 0 
+ *  * Return: Always 0 (Success/correct)
+ *
  */
 int main(void)
 {
-char alphabet;
+	char alphabet;
 
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-putchar(alphabet);
-}
-putchar('\n');
-return (0);
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
 }
