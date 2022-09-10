d75e1f787419c3821a1971665d2b066510b1ebd8
Ubaidullah Muhammad Ahmad <ubaidullahmahmad@gmail.com>
2022-09-10 03:19:15 -0700

    alx

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,15 @@
 /**
  *  * main - Entry point
  *
- *  * Return:1
+ *  * Return: returns 1
  *
  */
 int main(void)
-{
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, str1, sizeof(str1));
+{
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
+
 }
