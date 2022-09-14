0562a3b21aed18fde1178a1f7ccae6ef748f4fc6
Ubaidullah Muhammad Ahmad <ubaidullahmahmad@gmail.com>
2022-09-13 18:39:55 -0700

    nesting in C

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,22 +6,28 @@
  *
  * Return: Always 0
  */
-void print_to_98(void)
+void print_to_98(int n)
 {
-	if (n < 98)
+	if (n <= 98)
 	{
-		for (n = n; n < 98; n++)
+		for (; n <= 98; n++)
 		{
-			_putchar(n);
-			_putchar(98);
+			printf("%d", n);
+			if (n == 98)
+				continue;
+			printf(", ");
 		}
+		printf("\n");
 	}
 	else
 	{
-		for (n = n; n > 98; n--)
+		for (; n >= 98; n--)
 		{
-			_putchar(n);
-			_putchar(98);
+			printf("%d", n);
+			if (n == 98)
+				continue;
+			printf(", ");
 		}
+		printf("\n");
 	}
 }
