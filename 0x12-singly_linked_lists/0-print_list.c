8207b4a7aad5bc646f8159f67f824a75a7fe8e2e
Ubaidullah Muhammad Ahmad <ubaidullahmahmad@gmail.com>
2022-10-20 07:11:42 -0700

    lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- *  * print_list - prints all the elements  a list_t list.
+ *  * print_list - prints all the elements of a list_t list.
  *
  *   * @h: singly linked list.
  *
