188af09b7d85b43011f423d09f519418f28745b9
Ubaidullah Muhammad Ahmad <ubaidullahmahmad@gmail.com>
2022-10-25 14:20:15 -0700

    more links

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,24 +1,51 @@
 #include "lists.h"
 
 /**
- *  * sum_listint - calculates the sum of all the data in a listint_t list
- *
- *   * @head: first node in the linked list
- *
- *    *
- *
- *     * Return: resulting sum
- *
+ *  * insert_nodeint_at_index - inserts a new node
+ *   * at a given position.
+ *    * @head: head of a list.
+ *     * @idx: index of the list where the new node is
+ *      * added.
+ *       * @n: integer element.
+ *        *
+ *         * Return: the address of the new node, or NULL if it
+ *          * failed.
  */
-int sum_listint(listint_t *head)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	int sum = 0;
-	listint_t *temp = head;
+	unsigned int i;
+	listint_t *new;
+	listint_t *h;
 
-	while (temp)
+	h = *head;
+
+	if (idx != 0)
 	{
-		sum += temp->n;
-		temp = temp->next;
+		for (i = 0; i < idx - 1 && h != NULL; i++)
+		{
+			h = h->next;
+		}
 	}
-	return (sum);
+
+	if (h == NULL && idx != 0)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		new->next = h->next;
+		h->next = new;
+	}
+
+	return (new);
 }
