aaf06b5794fc4461643568ba39a9a883f4892f0b
Ubaidullah Muhammad Ahmad <ubaidullahmahmad@gmail.com>
2022-10-27 16:30:12 -0700

    BITWISE

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 
 /**
  * clear_bit - sets the value of a given bit to 0
@@ -9,8 +9,8 @@ i#include "main.h"
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
-	*n = (~(1UL << index) & *n);
+	*n = *n & ~(1 << index);
 	return (1);
 }
